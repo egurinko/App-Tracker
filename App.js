357c42062e253e2a80b4735ff6c454ee3a846640
egurinko <egctoru@gmail.com>
2019-01-06 01:12:38 +0900

    Play with tutorial

--- App.js
+++ App.js
@@ -1,49 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+import React from 'react';
+import {StyleSheet, Text, View, TextInput} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'powderblue'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  input: {
+    backgroundColor: 'red',
+    width: "100%"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  blinks: {
+    backgroundColor: 'pink',
+    flex: 1,
+    flexDirection: "row",
+    width: 50
+  }
 });
+
+class Blink extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+
+  render(){
+    return  <Text>{this.props.text}</Text>;
+  }
+}
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: ""
+    }
+  }
+
+  handleText = text => {
+    this.setState({text});
+  }
+
+  render() {
+    const { text } = this.state;
+    let blinks;
+    if(text.length !== 0){
+      blinks = text.split(" ")
+                   .map(text => <Blink style={styles.blinks} text={text} />)
+    }
+
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Type here"
+          onChangeText={this.handleText}
+        />
+        {blinks ? blinks : null}
+      </View>
+    );
+  }
+}
